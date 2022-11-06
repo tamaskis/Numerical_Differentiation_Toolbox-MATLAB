e279dd7c7ceb198ee5d8f3dac09315dd14e0b937
tamaskis <tamas.a.kis@outlook.com>
2022-11-05 18:47:57 -0700

    vector hessian tests

---
+++ tests/newscripts/TEST_hessian.m
@@ -0,0 +1,106 @@
+%% TEST_hessian.m
+% Numerical Differentiation Toolbox
+%
+% Unit testing of the chessian, fhessian, and ihessian functions.
+%
+% Copyright © 2021 Tamas Kis
+% Last Update: 2022-11-05
+% Website: https://tamaskis.github.io
+% Contact: tamas.a.kis@outlook.com
+%
+% DEPENDENCIES:
+%   • Simple Unit Testing Toolbox (https://github.com/tamaskis/Simple_Unit_TestinH_Toolbox-MATLAB)
+%   • Numerical Differentiation Toolbox (https://www.mathworks.com/matlabcentral/fileexchange/97267-numerical-differentiation-toolbox)
+
+
+
+%% SCRIPT SETUP
+
+% clears Workspace and closes all figures
+clear; close all;
+
+
+
+%% INSTANTIATE TEST SUITE
+
+test_suite = TestSuite('Hessian tests',false);
+
+
+
+%% UNIVARIATE, SCALAR-VALUED
+
+% test function
+f = @(x) x^3;
+
+% true Hessian
+H_true = @(x) 6*x;
+
+% numerical Hessians
+H_fd = @(x) fhessian(f,x);
+H_cd = @(x) chessian(f,x);
+H_cs = @(x) ihessian(f,x);
+
+% evaluation point
+x0 = 2;
+
+% unit tests
+test_suite.add_test(TestEqual(H_fd(x0),H_true(x0),'fhessian, univariate, scalar-valued',3));
+test_suite.add_test(TestEqual(H_cd(x0),H_true(x0),'chessian, univariate, scalar-valued',6));
+test_suite.add_test(TestEqual(H_cs(x0),H_true(x0),'ihessian, univariate, scalar-valued',10));
+
+
+
+%% MULTIVARIATE, SCALAR-VALUED #1
+
+% test function
+f = @(x) x(1)^2+x(2)^3;
+
+% true Hessian
+H_true = @(x) [2  0;
+               0  6*x(2)];
+
+% numerical Hessians
+H_fd = @(x) fhessian(f,x);
+H_cd = @(x) chessian(f,x);
+H_cs = @(x) ihessian(f,x);
+
+% evaluation point
+x0 = [1;
+      2];
+
+% unit tests
+test_suite.add_test(TestEqual(H_fd(x0),H_true(x0),'fhessian, multivariate, scalar-valued #1',3));
+test_suite.add_test(TestEqual(H_cd(x0),H_true(x0),'chessian, multivariate, scalar-valued #1',5));
+test_suite.add_test(TestEqual(H_cs(x0),H_true(x0),'ihessian, multivariate, scalar-valued #1',10));
+
+
+
+%% MULTIVARIATE, SCALAR-VALUED #2
+
+% test function
+f = @(x) x(1)^5*x(2)+x(1)*sin(x(2))^3;
+
+% true Hessian
+H_true = @(x) [20*x(1)^3*x(2),5*x(1)^4+3*sin(x(2))^2*cos(x(2));
+               5*x(1)^4+3*sin(x(2))^2*cos(x(2)),...
+               6*x(1)*sin(x(2))*cos(x(2))^2-3*x(1)*sin(x(2))^3];
+
+% numerical Hessians
+H_fd = @(x) fhessian(f,x);
+H_cd = @(x) chessian(f,x);
+H_cs = @(x) ihessian(f,x);
+
+% evaluation point
+x0 = [1;
+      2];
+
+% unit tests
+test_suite.add_test(TestEqual(H_fd(x0),H_true(x0),'fhessian, multivariate, scalar-valued #2',2));
+test_suite.add_test(TestEqual(H_cd(x0),H_true(x0),'chessian, multivariate, scalar-valued #2',4));
+test_suite.add_test(TestEqual(H_cs(x0),H_true(x0),'ihessian, multivariate, scalar-valued #2',7));
+
+
+
+%% RUNS TEST SUITE
+
+test_suite.run;
---
+++ tests/newscripts/TEST_vechessian.m
@@ -0,0 +1,136 @@
+%% TEST_vecvechessian.m
+% Numerical Differentiation Toolbox
+%
+% Unit testing of the cvechessian, fvechessian, and ivechessian functions.
+%
+% Copyright © 2021 Tamas Kis
+% Last Update: 2022-11-05
+% Website: https://tamaskis.github.io
+% Contact: tamas.a.kis@outlook.com
+%
+% DEPENDENCIES:
+%   • Simple Unit Testing Toolbox (https://github.com/tamaskis/Simple_Unit_TestinH_Toolbox-MATLAB)
+%   • Numerical Differentiation Toolbox (https://www.mathworks.com/matlabcentral/fileexchange/97267-numerical-differentiation-toolbox)
+
+
+
+%% SCRIPT SETUP
+
+% clears Workspace and closes all figures
+clear; close all;
+
+
+
+%% INSTANTIATE TEST SUITE
+
+test_suite = TestSuite('Hessian tests',false);
+
+
+
+%% UNIVARIATE, SCALAR-VALUED
+
+% test function
+f = @(x) x^3;
+
+% true vector Hessian
+H_true = @(x) 6*x;
+
+% numerical vector Hessians
+H_fd = @(x) fvechessian(f,x);
+H_cd = @(x) cvechessian(f,x);
+H_cs = @(x) ivechessian(f,x);
+
+% evaluation point
+x0 = 2;
+
+% unit tests
+test_suite.add_test(TestEqual(H_fd(x0),H_true(x0),'fvechessian, univariate, scalar-valued',3));
+test_suite.add_test(TestEqual(H_cd(x0),H_true(x0),'cvechessian, univariate, scalar-valued',6));
+test_suite.add_test(TestEqual(H_cs(x0),H_true(x0),'ivechessian, univariate, scalar-valued',10));
+
+
+
+%% MULTIVARIATE, SCALAR-VALUED #1
+
+% test function
+f = @(x) x(1)^2+x(2)^3;
+
+% true vector Hessian
+H_true = @(x) [2  0;
+               0  6*x(2)];
+
+% numerical vector Hessians
+H_fd = @(x) fvechessian(f,x);
+H_cd = @(x) cvechessian(f,x);
+H_cs = @(x) ivechessian(f,x);
+
+% evaluation point
+x0 = [1;
+      2];
+
+% unit tests
+test_suite.add_test(TestEqual(H_fd(x0),H_true(x0),'fvechessian, multivariate, scalar-valued #1',3));
+test_suite.add_test(TestEqual(H_cd(x0),H_true(x0),'cvechessian, multivariate, scalar-valued #1',5));
+test_suite.add_test(TestEqual(H_cs(x0),H_true(x0),'ivechessian, multivariate, scalar-valued #1',10));
+
+
+
+%% MULTIVARIATE, SCALAR-VALUED #2
+
+% test function
+f = @(x) x(1)^5*x(2)+x(1)*sin(x(2))^3;
+
+% true vector Hessian
+H_true = @(x) [20*x(1)^3*x(2),5*x(1)^4+3*sin(x(2))^2*cos(x(2));
+               5*x(1)^4+3*sin(x(2))^2*cos(x(2)),...
+               6*x(1)*sin(x(2))*cos(x(2))^2-3*x(1)*sin(x(2))^3];
+
+% numerical vector Hessians
+H_fd = @(x) fvechessian(f,x);
+H_cd = @(x) cvechessian(f,x);
+H_cs = @(x) ivechessian(f,x);
+
+% evaluation point
+x0 = [1;
+      2];
+
+% unit tests
+test_suite.add_test(TestEqual(H_fd(x0),H_true(x0),'fvechessian, multivariate, scalar-valued #2',2));
+test_suite.add_test(TestEqual(H_cd(x0),H_true(x0),'cvechessian, multivariate, scalar-valued #2',4));
+test_suite.add_test(TestEqual(H_cs(x0),H_true(x0),'ivechessian, multivariate, scalar-valued #2',7));
+
+
+
+%% MULTIVARIATE, VECTOR-VALUED
+
+% test function
+f = @(x) [x(1)^5*x(2)+x(1)*sin(x(2))^3;
+          x(1)^3+x(2)^4-3*x(1)^2*x(2)^2];
+
+% true vector Hessians
+H1 = @(x) [20*x(1)^3*x(2),5*x(1)^4+3*sin(x(2))^2*cos(x(2));
+           5*x(1)^4+3*sin(x(2))^2*cos(x(2)),...
+           6*x(1)*sin(x(2))*cos(x(2))^2-3*x(1)*sin(x(2))^3];
+H2 = @(x) [6*x(1)^2-6*x(2)^2  -12*x(1)*x(2);
+           -12*x(1)*x(2)       12*x(2)^2-6*x(1)^2];
+H_true = @(x) cat(3,H1(x),H2(x));
+
+% numerical vector Hessians
+H_fd = @(x) fvechessian(f,x);
+H_cd = @(x) cvechessian(f,x);
+H_cs = @(x) ivechessian(f,x);
+
+% evaluation point
+x0 = [1;
+      2];
+
+% unit tests
+test_suite.add_test(TestEqual(H_fd(x0),H_true(x0),'fvechessian, multivariate, scalar-valued #2',2));
+test_suite.add_test(TestEqual(H_cd(x0),H_true(x0),'cvechessian, multivariate, scalar-valued #2',4));
+test_suite.add_test(TestEqual(H_cs(x0),H_true(x0),'ivechessian, multivariate, scalar-valued #2',7));
+
+
+
+%% RUNS TEST SUITE
+
+test_suite.run;
